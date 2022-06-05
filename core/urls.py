868e21646b38127e9d524b2976fcd02deacd7fb3
VelisRB <jvelis.vlz@gmail.com>
2022-06-05 17:35:19 -0400

    Linkeo CSS

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from .views import index, personalizados, galeria, aboutus, sugerencias, arnesca
 
 urlpatterns = [
     path('index.html', index,name="index"),
+    path('', index,name="index"),
     path('personalizados.html', personalizados, name="personalizados"),
     path('galeria.html', galeria, name="galeria"),
     path('aboutus.html', aboutus, name="aboutus"),
