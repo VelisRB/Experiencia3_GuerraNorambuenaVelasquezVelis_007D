e7eeeb934bff28c03ae9164593820493952fc054
Nicolás Norambuena <ni.norambuena@duocuc.cl>
2022-06-05 19:21:35 -0400

    Implementando correcciones en pagina Galeria.html

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, personalizados, galeria, aboutus, sugerencias, arnescafe, arnesverde, collarcafe, collarmorado, collarrojo, collarverde, correanegra, correarosa
+from .views import index, personalizados, galeria, aboutus, sugerencias
 
 urlpatterns = [
     path('index.html', index,name="index"),
@@ -8,14 +8,5 @@ urlpatterns = [
     path('galeria.html', galeria, name="galeria"),
     path('aboutus.html', aboutus, name="aboutus"),
     path('sugerencias.html', sugerencias, name="sugerencias"),
-    path('core/catalogo/arnescafe.html', arnescafe, name="arnescafe"),
-    path('core/catalogo/arnesverde.html', arnesverde, name="arnesverde"),
-    path('core/catalogo/collarcafe.html', collarcafe, name="collarcafe"),
-    path('core/catalogo/collarmorado.html', collarmorado, name="collarmorado"),
-    path('core/catalogo/collarrojo.html', collarrojo, name="collarrojo"),
-    path('core/catalogo/collarverde.html', collarverde, name="collarverde"),
-    path('core/catalogo/correanegra.html', correanegra, name="correanegra"),
-    path('core/catalogo/correarosa.html', correarosa, name="correarosa"),
-
 
 ]
--- core/views.py
+++ core/views.py
@@ -17,29 +17,6 @@ def aboutus(request):
 def sugerencias(request):
     return render(request, 'core/sugerencias.html')
 
-def arnescafe(request):
-    return render(request, 'core/catalogo/arnescafe.html')
-
-def arnesverde(request):
-    return render(request, 'core/catalogo/arnesverde.html')
-
-def collarcafe(request):
-    return render(request, 'core/catalogo/collarcafe.html')
-
-def collarmorado(request):
-    return render(request, 'core/catalogo/collarmorado.html')
-
-def collarrojo(request):
-    return render(request, 'core/catalogo/collarrojo.html')
-
-def collarverde(request):
-    return render(request, 'core/catalogo/collarverde.html')
-
-def correanegra(request):
-    return render(request, 'core/catalogo/correanegra.html')
-
-def correarosa(request):
-    return render(request, 'core/catalogo/correarosa.html')
 
     
 
